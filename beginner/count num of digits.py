dfcff47baf39720477a1ac6c165b78553b2a5511
sankar55 <39687336+sankar55@users.noreply.github.com>
2018-05-28 12:53:34 +0530

    Update count num of digits.py

--- beginner/count num of digits.py
+++ beginner/count num of digits.py
@@ -1,6 +1,6 @@
-n=int(input("Enter number:"))
+num=int(input("Enter number:"))
 count=0
-while(n>0):
+while(num>0):
     count=count+1
-    n=n//10
+    num=num//10
 print("The number of digits in the number are:",count)
