b4ce9ed325699c68c8373d040f7b2bd3ba3e96db
sankar55 <39687336+sankar55@users.noreply.github.com>
2018-05-28 10:52:13 +0530

    Update largest number.py

--- beginner/largest number.py
+++ beginner/largest number.py
@@ -2,7 +2,7 @@ a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 c = float(input("Enter third number: "))
  
-if (a > b) and (a > b):
+if (a > b) and (a > c):
    largest = a
 elif (b> a) and (b> c):
    largest = b
